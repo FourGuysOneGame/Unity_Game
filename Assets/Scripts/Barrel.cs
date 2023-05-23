18bd4aedda56e8f225ec9df78bf8fb868bea8048
Kamil Deren <kamil.deren1@gmail.com>
2023-05-23 16:01:51 +0200

    Added "Life Heart" drop from barrels

---
+++ Assets/Scripts/Barrel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.LowLevel;
+
+
+public class Barrel : MonoBehaviour, IHitable
+{
+    public List<GameObject> itemDrops;
+    private int barrelHp = 3;
+
+    private void ItemDrop()
+    {
+        if (barrelHp == 0)
+        {
+            for (int i = 0; i < itemDrops.Count; i++)
+            {
+                Debug.Log(itemDrops);
+                Instantiate(itemDrops[i], transform.position, Quaternion.identity);
+                Debug.Log("Item dropped");
+            }
+
+            Destroy(gameObject);
+        }
+        else
+            barrelHp--;
+    }
+    
+    public void TakeHit()
+    {
+        ItemDrop();
+    }
+}
---
+++ Assets/Scripts/ItemDrop.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public class ItemDrop : MonoBehaviour
+{
+    private Rigidbody2D itemRb;
+    public float dropForce = 2;
+
+    private void Start()
+    {
+        itemRb = GetComponent<Rigidbody2D>();
+        itemRb.AddForce(Vector2.up * dropForce, ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<Player>().Actions.Heal();
+            Destroy(gameObject);
+        }
+    }
+}
