157032d73b1da09e07ffdb5ab1ede7c16945c115
Kamil Deren <kamil.deren1@gmail.com>
2023-05-23 12:03:42 +0200

    Added basic next level doors that "open" when enemies killed.
    To make it work with multiple levels you need to:
    1. Go to File>Build settings and add all levels there (look at indexes)
    2. Create "doors" with BoxCollider2D (IsTrigger on), LevelController.cs attached and set Index variable to index of level you want to go to

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+    // Number of level, if we want to be able to go back in levels need to make it a parameter instead of static 1++
+    public int index;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && Enemy.enemiesAmount <= 0)
+            SceneManager.LoadScene(index);
+    }
+}
