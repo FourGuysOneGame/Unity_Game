b86b1224ef3f7b2e80eed4cd06d3819b8aee1e53
Kamil Deren <kamil.deren1@gmail.com>
2023-06-07 18:09:38 +0200

    Added fading background Refactored Door prefab so it works with fading background Changed ladder in lvl3 (it was not working :) )

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -1,17 +1,29 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelController : MonoBehaviour
 {
     // Number of level, if we want to be able to go back in levels need to make it a parameter instead of static 1++
     public int index = 1;
+    public Image black;
+    public Animator anim;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player") && Enemy.enemiesAmount <= 0)
         {
+            StartCoroutine(Fading());
             SceneManager.LoadScene(index);
-            index = (index + 1) % 3;
         }
+    }
+
+    IEnumerator Fading()
+    {
+        anim.SetBool("Fade", true);
+        yield return new WaitUntil(() => black.color.a == 1);
     }
 }
